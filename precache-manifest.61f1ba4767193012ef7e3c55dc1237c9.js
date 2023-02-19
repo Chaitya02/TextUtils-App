977e7a2b5d83a11e74ce770a548c74f26a26e915
Chaitya <chaityadobariya02@gmail.com>
2023-02-19 15:06:30 +0530

    Updates

---
+++ precache-manifest.61f1ba4767193012ef7e3c55dc1237c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67c0dbc53df0ae5fb089",
+    "url": "/TextUtils-App/static/css/main.24a65c73.chunk.css"
+  },
+  {
+    "revision": "67c0dbc53df0ae5fb089",
+    "url": "/TextUtils-App/static/js/main.67c0dbc5.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-App/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-App/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "a8912552b6378c710684",
+    "url": "/TextUtils-App/static/js/runtime~main.a8912552.js"
+  },
+  {
+    "revision": "6bfbafecff0efe7c91b7ff9664342ec3",
+    "url": "/TextUtils-App/index.html"
+  }
+];
